cfce475b0cd9ccd578da221c2e67ae4a310e21af
Eugene Roots <eugenehespect@gmail.com>
2022-08-20 20:53:08 +0300

    Add test scenario with other data (test_make_an_order.py)

--- tests/test_make_an_order.py
+++ tests/test_make_an_order.py
@@ -4,7 +4,7 @@ import allure
 
 
 @allure.title('Проверка формы заказа самоката.')
-def test_make_new_order(driver):
+def test_make_new_order_ivan(driver):
     homepage_actions = HomepageActions(driver)
     order_scooter = OrderScooterFormActions(driver)
     homepage_actions.click_order_button()
@@ -23,3 +23,25 @@ def test_make_new_order(driver):
     order_scooter.wait_order_success_popup_is_displayed()
     order_scooter.click_button_see_order_status()
     assert order_scooter.customer_name(name="Иван") == "Иван", "Имя заказчика не совпадает с именем показанным на экране!"
+
+
+@allure.title('Проверка формы заказа самоката.')
+def test_make_new_order_oleg(driver):
+    homepage_actions = HomepageActions(driver)
+    order_scooter = OrderScooterFormActions(driver)
+    homepage_actions.click_order_button()
+    order_scooter.wait_order_form_is_displayed()
+    order_scooter.fill_for_whom_section(
+        name="Олег",
+        surname="Петров",
+        address="ул. Баширова, д. Петрова",
+        phone="98765432101")
+    order_scooter.click_button_next()
+    order_scooter.wait_about_order_form_is_displayed()
+    order_scooter.fill_about_order_section(comment="Авто-тест")
+    order_scooter.click_button_complete_order()
+    order_scooter.wait_complete_order_popup_is_displayed()
+    order_scooter.click_button_yes()
+    order_scooter.wait_order_success_popup_is_displayed()
+    order_scooter.click_button_see_order_status()
+    assert order_scooter.customer_name(name="Олег") == "Олег", "Имя заказчика не совпадает с именем показанным на экране!"
